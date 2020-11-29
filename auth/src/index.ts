37ac00d3f08d356dc2caf367315acf0518d175ff
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-11-29 16:11:47 -0300

    ingress-kubernetes configuration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (request, response) => {
+  return response.send('Hi there!!')
+})
+
 app.listen(3000, () => {
   console.log(`Auth Service listening on PORT 3000!!`)
 })
