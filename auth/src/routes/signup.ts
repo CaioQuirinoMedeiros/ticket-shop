7c4c633b56e6fb4c68ebe2be9431d08c632d4897
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-11-30 14:57:27 -0300

    using express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.get('/api/users/signup', (request, response) => {
-  return response.send('Hi there signup!!')
-})
+router.post(
+  '/api/users/signup',
+  [
+    body('email', 'Please, provide a valid e-mail').isEmail().normalizeEmail(),
+    body('password', 'Password must be between 6 and 24 characters').isLength({
+      min: 6,
+      max: 24
+    })
+  ],
+  (request: Request, response: Response) => {
+    const errors = validationResult(request)
 
-export {router as signupRouter}
+    if (!errors.isEmpty()) {
+      return response.status(400).send({ errors: errors.array() })
+    }
+
+    const { email, password } = request.body
+
+    console.log('Creating user...')
+    return response.send({ email, password })
+  }
+)
+
+export { router as signupRouter }
