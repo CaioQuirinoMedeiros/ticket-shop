d167880267ee4f9f841b9a24d6323f4f87d99bdf
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-12-05 01:55:17 -0300

    signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
+import ValidationError from '../errors/ValidationError'
+import DatabaseError from '../errors/DatabaseError'
+import { User } from '../models/user'
+import AppError from '../errors/AppError'
+import NotFoundError from '../errors/NotFoundError'
+import { Password } from '../services/password'
 
 const router = express.Router()
 
-router.get('/api/users/signin', (request, response) => {
-  return response.send('Hi there, signin!!')
-})
+router.post(
+  '/api/users/signin',
+  [
+    body('email', 'E-mail must be valid').isEmail(),
+    body('password', 'Provide your password').trim().notEmpty()
+  ],
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request)
+
+    if (!errors.isEmpty()) {
+      throw new ValidationError(errors.array())
+    }
+
+    const { email, password } = request.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      throw new NotFoundError('User not found')
+    }
+
+    const passwordMatch = await Password.compare(user.password, password)
+
+    if (!passwordMatch) {
+      throw new AppError('Invalid credentials', 403)
+    }
+
+    const userJWT = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    )
+
+    request.session = { jwt: userJWT }
+
+    return response.status(200).send({ user })
+  }
+)
 
 export { router as signinRouter }
